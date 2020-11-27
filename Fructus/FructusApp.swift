ab5e93f288e262d6d3e7eb8ce0bdbd2586d58077
Etoka Kingsley <etokakingsley@gmail.com>
2020-11-27 05:47:05 +0100

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Kingsley Etoka on 27/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
